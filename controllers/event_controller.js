303ed810167d8b1f2ab751bc999c7d2aa8b0e196
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2024-03-26 11:32:55 +0200

    Update event_controller.js

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -11,3 +11,18 @@ exports.getAllEvents = async (req, res) => {
     res.status(400).send(error); 
   }
 };
+
+
+// Get event by ID
+exports.getEventById = async (req, res) => {
+  try {
+    const eventId = req.params.id;
+    const event = await Event.findById(eventId);
+    if (!event) {
+      return res.status(404).send({ message: 'Event not found' });
+    }
+    res.status(200).send(event);
+  } catch (error) {
+    res.status(400).send(error); 
+  }
+};
