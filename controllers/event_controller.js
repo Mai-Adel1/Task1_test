79c3cf1081b900a88563fc7ca6c6eae2e2bbc9cd
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2024-03-26 11:32:22 +0200

    Update event_controller.js

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -1,3 +1,13 @@
 const { Event } = require('../models/event');
 const Joi = require('joi');
 const { validateEvent } = require('../helper/validation'); 
+
+// Get all events
+exports.getAllEvents = async (req, res) => {
+  try {
+    const allEvents = await Event.find({});
+    res.status(200).send(allEvents);
+  } catch (error) {
+    res.status(400).send(error); 
+  }
+};
